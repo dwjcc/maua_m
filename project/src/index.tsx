d4271214ed512f7f6030f00760224743cb50eaf0
daniela <daniela.140872@gmail.com>
2025-05-15 18:40:03 -0300

    APP: Login screen

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { RecuperarSenha } from "./screens/RecuperarSenha";
+import { Login } from "./screens/Login";
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
     <BrowserRouter>
       <RecuperarSenha />
+      <Login />
     </BrowserRouter>
   </StrictMode>,
 );
