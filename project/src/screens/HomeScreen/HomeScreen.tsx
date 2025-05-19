c3d66493e5d434ca3c5ab27b2e6a75249d075a7b
daniela <daniela.140872@gmail.com>
2025-05-19 19:20:01 -0300

    APP: Google maps api

--- project/src/screens/HomeScreen/HomeScreen.tsx
+++ project/src/screens/HomeScreen/HomeScreen.tsx
@@ -1,6 +1,17 @@
 import { useNavigate } from "react-router-dom";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 import { Button } from "../../components/ui/button";
 import { Card } from "../../components/ui/card";
+
+const defaultCenter = {
+  lat: -23.6486, // Instituto Mauá de Tecnologia coordinates
+  lng: -46.5752
+};
+
+const mapStyles = {
+  height: "100%",
+  width: "100%"
+};
 
 export const HomeScreen = (): JSX.Element => {
   const navigate = useNavigate();
@@ -8,7 +19,17 @@ export const HomeScreen = (): JSX.Element => {
   return (
     <div className="bg-white flex flex-row justify-center w-full">
       <div className="bg-white w-[390px] h-[844px]">
-        <div className="relative h-[844px] bg-[url(/map.svg)] bg-cover bg-center">
+        <div className="relative h-[844px]">
+          <LoadScript googleMapsApiKey="AIzaSyCFGRp2TK00A43bbj9gGYT8I-tP-TO5FRk">
+            <GoogleMap
+              mapContainerStyle={mapStyles}
+              zoom={15}
+              center={defaultCenter}
+            >
+              <Marker position={defaultCenter} />
+            </GoogleMap>
+          </LoadScript>
+
           {/* Enter Queue Button */}
           <Button 
             onClick={() => navigate("/queue")}
@@ -21,16 +42,16 @@ export const HomeScreen = (): JSX.Element => {
 
           {/* Location Button */}
           <Card 
-            className="absolute w-16 h-16 top-[104px] right-[16px] bg-[#d9d9d9] rounded-[32px] flex items-center justify-center p-0 border-none cursor-pointer hover:bg-[#c9c9c9]"
-            onClick={() => {/* Add location functionality here */}}
+            className="absolute w-16 h-16 top-[104px] right-[16px] bg-[#d9d9d9] rounded-[32px] flex items-center justify-center p-0 border-none cursor-pointer hover:bg-[#c9c9c9]" 
+            onClick={() => alert("Location functionality coming soon!")}
           >
             <div className="relative w-[39px] h-[49px] bg-[url(/group-3.png)] bg-cover" />
           </Card>
 
           {/* User Profile Button */}
           <Card 
-            className="absolute w-16 h-16 top-[29px] right-[16px] bg-[#d9d9d9] rounded-[32px] flex items-center justify-center p-0 border-none cursor-pointer hover:bg-[#c9c9c9]"
             onClick={() => navigate("/profile")}
+            className="absolute w-16 h-16 top-[29px] right-[16px] bg-[#d9d9d9] rounded-[32px] flex items-center justify-center p-0 border-none cursor-pointer hover:bg-[#c9c9c9]"
           >
             <img
               className="w-[53px] h-[53px]"
@@ -38,13 +59,6 @@ export const HomeScreen = (): JSX.Element => {
               src="/vector.svg"
             />
           </Card>
-
-          {/* Map Pin */}
-          <img
-            className="absolute w-[30px] h-8 top-[300px] left-[162px]"
-            alt="Location pin"
-            src="/vector-1.svg"
-          />
         </div>
       </div>
     </div>
